c1cf088702b3c880658c070c5e3bf7efaa155fe6
jixingx <1544916656@qq.com>
2020-02-11 17:40:51 +0800

    第一次提交，配置了vue,vant,fastClick环境

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+//引入重置css样式
+import "./assets/style/resets.css"
+
+import fastClick from 'fastclick' //300毫秒点击延迟问题的解决方案（尤其是移动端）优化用户体验
+
+fastClick.attach(document.body);//fastclick自带的一个方法
+
+//vant
+import { Button } from 'vant';
+Vue.use(Button);
+
+
+
+
 new Vue({
   router,
   store,
